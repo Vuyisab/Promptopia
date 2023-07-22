4b4fb20b39db236a9bd621fc19de978e93a7de8e
Vuyisa Baza <vuyisa.baza@entelect.co.za>
2023-07-22 17:47:04 +0200

    finished setting up initial auth

---
+++ utils/database.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const isConnected = false;
+
+export const connectToDatabase = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URi, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
